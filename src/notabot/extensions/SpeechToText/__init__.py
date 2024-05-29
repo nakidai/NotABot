909b271f70246f096577e51520cac8ccd7206594
Nakidai <plaza521@inbox.ru>
2024-05-29 15:00:58 +0300

    Add SpeechToText cog
    This cog will retrieve some text from voice messages

---
+++ src/notabot/extensions/SpeechToText/__init__.py
@@ -0,0 +1,34 @@
+import discord
+import speech_recognition as sr
+from discord.ext import commands
+from pydub import AudioSegment
+
+import os.path as p
+
+class Cog(commands.Cog, name="SpeechToTextCog"):
+    def __init__(self, client: commands.Bot) -> None:
+        self.client = client
+
+    @commands.Cog.listener(name="on_message")
+    async def speech_to_text(
+        self,
+        message: discord.Message
+    ) -> None:
+        print(p.dirname(sr.__file__))
+        if message.attachments and (att := message.attachments[0]).is_voice_message():
+            ogg_path = self.client.path(f"var/{att.filename}")
+            wav_path = self.client.path(f"var/{att.filename}".replace(".ogg", ".wav"))
+            await att.save(ogg_path)
+            ogg = AudioSegment.from_file(ogg_path)
+            ogg.export(self.client.path(wav_path), format="wav")
+
+            recognizer = sr.Recognizer()
+            with sr.AudioFile(wav_path) as wav:
+                voice = recognizer.record(wav)
+
+            try:
+                await message.reply(recognizer.recognize_sphinx(voice, language="ru-RU"))
+            except sr.UnknownValueError:
+                await message.reply("Good!")
+            except Exception as exc:
+                print(f"Cannot respond to voice message: {exc}")
