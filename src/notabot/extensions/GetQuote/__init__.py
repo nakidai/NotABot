a618ee2e7d0557ccf372a3a3ad10216aff5aad60
VengDevs <111250418+VengDevs@users.noreply.github.com>
2024-07-16 03:01:04 +0300

    updated key

--- src/notabot/extensions/GetQuote/__init__.py
+++ src/notabot/extensions/GetQuote/__init__.py
@@ -9,6 +9,7 @@ from discord import app_commands
 class Cog(commands.Cog, name="GetQuoteCog"):
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
+        self.qapikey = ""
 
     @app_commands.command(
         name="randquote",
@@ -18,8 +19,14 @@ class Cog(commands.Cog, name="GetQuoteCog"):
         self,
         interaction: discord.Interaction
     ) -> None:
+        if self.qapikey == "":
+            await interaction.response.send_message(
+                "Use /setqapikey to set quote APi key, please.\nGet API key - https://api-ninjas.com/api/quotes",
+                ephemeral=True
+            )
+            return
         api_url = "https://api.api-ninjas.com/v1/quotes?category="
-        response = requests.get(api_url, headers={'X-Api-Key': 'RYG3jNHiGlbfWOJPry9q6g==XNOxVtdu8S17eYJZ'})
+        response = requests.get(api_url, headers={"X-Api-Key": self.qapikey})
         quote: dict
 
         if response.status_code == requests.codes.ok:
@@ -37,5 +44,24 @@ class Cog(commands.Cog, name="GetQuoteCog"):
         await interaction.response.send_message(
                 f"{text}\n-# {author} - {category}",
                 ephemeral=False
+            )
+        return
+    
+    @app_commands.command(
+        name="setqapikey",
+        description="Set quote API key"
+    )
+    @app_commands.describe(
+        key="Key"
+    )
+    async def setqapikey(
+        self,
+        interaction: discord.Interaction,
+        key: str
+    ) -> None:
+        self.qapikey = key
+        await interaction.response.send_message(
+                "Key applied",
+                ephemeral=True
             )
         return
